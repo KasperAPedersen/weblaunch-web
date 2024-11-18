b4076fdf6c6d62c68aeb336d865ad38165102a9c
Kasper Doe <kasper@doe.dk>
2024-11-18 15:21:10 +0100

    .

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 import Express from 'npm:express';
-import https from 'https';
-import fs from 'fs';
-import path from 'path';
 
 const app = new Express();
 
-// SSL Certificates
-const sslOptions = {
-    key: fs.readFileSync(path.resolve('path/to/your/private.key')),
-    cert: fs.readFileSync(path.resolve('path/to/your/certificate.crt'))
-};
-
 app.use(Express.static('public'));
 
-https.createServer(sslOptions, app).listen(3000, () => {
-    console.log('Server started on https://localhost:3000');
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
 });
