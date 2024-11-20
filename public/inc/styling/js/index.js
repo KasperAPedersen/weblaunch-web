b50068ff792064a5a0dbbd833231e659955c9c4f
Kasper Doe <kasper@doe.dk>
2024-11-20 12:31:22 +0100

    Added styling and functionality to mobile navigation

--- public/inc/styling/js/index.js
+++ public/inc/styling/js/index.js
@@ -1,17 +1,30 @@
-let navigationLinks = document.getElementById('headerNav').getElementsByTagName('a');
+let navigationLinks = document.getElementsByClassName('navBtn');
+let dialogBox = document.getElementById('mobileNav').getElementsByTagName('dialog')[0];
 
 for(let i = 0; i < navigationLinks.length; i++) {
     navigationLinks[i].addEventListener('click', function (e) {
         e.preventDefault();
-
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
         const offset = 125;
-        console.log(targetElement.offsetTop);
 
         window.scrollTo({
             top: targetElement.offsetTop - offset,
             behavior: 'smooth'
         });
+
+        if(dialogBox.open) dialogBox.close();
+        for(let i = 0; i < navigationLinks.length; i++) navigationLinks[i].classList = "navBtn";
+        e.target.classList = "headerNavActive navBtn";
     });
+}
+
+let mobileNavToggle = () => {
+    if(dialogBox.open) {
+        dialogBox.close();
+        dialogBox.style.display = "none";
+    } else {
+        dialogBox.showModal();
+        dialogBox.style.display = "grid";
+    }
 }
