778d0a610dfe0357c5a83379edc48973dc4915fc
Kasper Alexander Pedersen <kasperpedersen1999@gmail.com>
2024-11-23 17:30:56 +0100

    Added nullable to price in prices model
    Added name field to prices model

--- orm/Models/Prices.js
+++ orm/Models/Prices.js
@@ -7,13 +7,17 @@ let Prices = sequelize.define('Prices', {
         autoIncrement: true,
         primaryKey: true
     },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
     description: {
         type: DataTypes.STRING,
         allowNull: false
     },
     price:{
-        type: DataTypes.DECIMAL,
-        allowNull: false
+        type: DataTypes.DECIMAL(10, 3),
+        allowNull: true
     }
 }, {
     tableName: 'Prices',
